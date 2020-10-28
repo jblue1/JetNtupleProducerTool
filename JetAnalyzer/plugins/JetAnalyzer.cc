fef23e57e2b2035add939f56b7e0d7d6057c6913
Braden <brkronheim@davidson.edu>
2020-10-28 16:33:03 -0400

    typos

--- JetAnalyzer/plugins/JetAnalyzer.cc
+++ JetAnalyzer/plugins/JetAnalyzer.cc
@@ -604,7 +604,7 @@ void JetAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
         } customLess;*/
 		
 		
-		const std::vector<pat::PackedCandidate> pfsVector =  dynamic_cast<const std::vector<pat::PackedCandidate>>(pfs);
+		const std::vector<pat::PackedCandidate> &pfsVector =  dynamic_cast<const std::vector<pat::PackedCandidate> &>(pfs);
 		
 		std::cout << typeid(pfs).name() << std::endl;
 		std::cout << typeid(pfsVector).name() << std::endl;
